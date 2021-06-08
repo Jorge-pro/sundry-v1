00d186207461aef0ed83c8b20514f0d39440f772
jorge-pro <jorge.rs.poa@hotmail.com>
2021-06-08 13:03:43 -0300

    Created Site Sundry - V.1

---
+++ frontend/src/Components/Navbar/MenuItems.js
@@ -0,0 +1,31 @@
+
+export const MenuItems = [
+ 
+  {
+    title: 'Home',
+    url: '#',
+    cName: 'nav-links',
+  },
+  
+  {
+    title: 'How it Works',
+    url: '#',
+    cName: 'nav-links',
+  },
+  {
+    title: 'Category',
+    url: '#',
+    cName: 'nav-links',
+  },
+  {
+    title: 'Contacts Us',
+    url: '#',
+    cName: 'nav-links',
+  },
+ {
+  title: 'Create an account',
+    url: '#',
+    cName: 'nav-links-mobile',
+  }
+]
+
